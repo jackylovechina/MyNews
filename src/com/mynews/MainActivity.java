cab2a32d573df3390c655a635584df3603022fb3
jackylovechina <Administrator@USER-20160628LK.localdomain>
2016-07-15 09:08:46 +0800

    use webview test

--- src/com/mynews/MainActivity.java
+++ src/com/mynews/MainActivity.java
@@ -1,34 +1,66 @@
 package com.mynews;
 
 import android.app.Activity;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	private WebView webView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		webView = (WebView) findViewById(R.id.webView);
+		webView.getSettings().setJavaScriptEnabled(true);
+
+		webView.loadUrl("http://hao123.com");
+
+		webView.setWebViewClient(new WebViewClient() {
+
+			// 覆盖WebView默认使用第三方或系统默认浏览器打开网页的行为，使网页用WebView打开
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				// TODO Auto-generated method stub
+				// 返回值是true的时候控制去WebView打开，为false调用系统浏览器或第三方浏览器
+				view.loadUrl(url);
+				return true;
+			}
+		});
+
+		// 可以打开https页面
+		webView.setWebViewClient(new WebViewClient() {
+
+			@Override
+			public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+				handler.proceed();
+				// handler.cancel();
+				// handler.handleMessage(null);
+			}
+		});
 	}
 
+	// 改写物理按键——返回的逻辑
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		if (keyCode == KeyEvent.KEYCODE_BACK) {
+			if (webView.canGoBack()) {
+				webView.goBack();// 返回上一页面
+				return true;
+			} else {
+				System.exit(0);// 退出程序
+			}
 		}
-		return super.onOptionsItemSelected(item);
+		return super.onKeyDown(keyCode, event);
 	}
+
 }
